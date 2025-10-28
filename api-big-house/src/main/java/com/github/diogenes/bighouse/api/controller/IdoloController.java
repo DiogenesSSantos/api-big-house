b3aeaaeb00a1bf1851fb8b67fd9c969de62c8e98
Diogenes santos <diogenescontatoofficial@hotmail.com>
2025-10-27 22:04:35 -0300

    Criando o método post, para salva objeto idolo no banco de dados, sem obrigatóriedade da foto.

--- api-big-house/src/main/java/com/github/diogenes/bighouse/api/controller/IdoloController.java
+++ api-big-house/src/main/java/com/github/diogenes/bighouse/api/controller/IdoloController.java
@@ -4,17 +4,13 @@ package com.github.diogenes.bighouse.api.controller;
 import com.github.diogenes.bighouse.api.model.Idolo;
 import com.github.diogenes.bighouse.api.service.IdoloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.spi.ImageReaderSpi;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import static org.springframework.http.MediaType.IMAGE_PNG;
 
@@ -29,14 +25,6 @@ public class IdoloController {
     public ResponseEntity<?> buscarTodas() throws IOException {
 
 
-        InputStream in = IdoloController.class.getResourceAsStream("/image/Captura de tela 2025-10-25 230354.png");
-        Idolo idolo = new Idolo.builder()
-                .setFrase("Dioge")
-                .setImage(in != null ? in.readAllBytes() : null)
-                .build();
-
-        service.salvar(idolo);
-
         return ResponseEntity.ok().body("Salvo");
     }
 
@@ -44,8 +32,6 @@ public class IdoloController {
     @GetMapping(value = "/buscar-foto/{id}" , produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<?> buscarFoto(@PathVariable ("id") Long id) {
         Idolo IdoloLocalizado = service.buscarPorId(id);
-
-        System.out.println(IdoloLocalizado.getFrase());
 
         return ResponseEntity.ok()
                 .contentType(IMAGE_PNG)
@@ -61,7 +47,28 @@ public class IdoloController {
     }
 
 
+    @PostMapping("/salva-postman")
+    public ResponseEntity<?> salvarPostman(String frase ,
+                                           @RequestParam(value = "file", required = false) MultipartFile file) throws IOException {
+        var idolo = instancieIdolo(frase, file);
+        service.salvar(idolo);
 
+        return ResponseEntity.ok().body(idolo);
+    }
+
+
+    /**
+     *
+     * @param frase frase impactante do idolo para ser persistida no banco de dados
+     * @param file é imagen, não e obrigatório
+     * @return retornado um objeto de idolo
+     * @throws IOException ao trabalhar com Input e Output somos obrigados a tratar exception.
+     */
+    private static Idolo instancieIdolo(String frase, MultipartFile file) throws IOException {
+        return new Idolo.builder().setFrase(frase)
+                .setImage(file == null ? null : file.getBytes())
+                .build();
+    }
 
 
 }
--- api-big-house/src/main/java/com/github/diogenes/bighouse/api/exceptionhandler/ExceptIonHandlerAPI.java
+++ api-big-house/src/main/java/com/github/diogenes/bighouse/api/exceptionhandler/ExceptIonHandlerAPI.java
@@ -4,7 +4,7 @@ package com.github.diogenes.bighouse.api.exceptionhandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class ExcepitonHandlerAPI {
+public class ExceptIonHandlerAPI {
 
 
 
